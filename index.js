44441cd688d6c331e3c39f91030f75e4107951cd
Ye Htet <yhtet1934@gmail.com>
2023-10-25 19:57:27 +0630

    complete object lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jame",
+    streetAddress: "365, A1 Block"
+}
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    return {
+        ...employeeObj, 
+        [key]: value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const clone = Object.assign({}, obj);
+    delete clone[key];
+    return clone
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
